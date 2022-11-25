7690edfecb648af2c39e416654d47bcebed089aa
William Lines <williamlines42@gmail.com>
2022-11-25 10:58:03 +0000

    added links to titles and thumbnail images

--- __mocks__/apiMock.js
+++ __mocks__/apiMock.js
@@ -1,140 +1,170 @@
-const apiMock = {
+apiMock = {
   "response": {
     "status": "ok",
     "userTier": "developer",
-    "total": 2389140,
+    "total": 2389293,
     "startIndex": 1,
     "pageSize": 10,
     "currentPage": 1,
-    "pages": 238914,
+    "pages": 238930,
     "orderBy": "newest",
     "results": [
       {
-        "id": "football/live/2022/nov/24/portugal-v-ghana-world-cup-2022-live",
+        "id": "business/live/2022/nov/25/black-friday-retailers-bleak-friday-cost-of-living-strike-amazon-economy-hospitality-business-live",
+        "type": "liveblog",
+        "sectionId": "business",
+        "sectionName": "Business",
+        "webPublicationDate": "2022-11-25T10:31:40Z",
+        "webTitle": "Retailers face ‘Bleak Friday’ as cost of living crisis hits spending – business live",
+        "webUrl": "https://www.theguardian.com/business/live/2022/nov/25/black-friday-retailers-bleak-friday-cost-of-living-strike-amazon-economy-hospitality-business-live",
+        "apiUrl": "https://content.guardianapis.com/business/live/2022/nov/25/black-friday-retailers-bleak-friday-cost-of-living-strike-amazon-economy-hospitality-business-live",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/b3e62b90c92d805fadd5c325aca55d078bce77a6/0_79_4837_2901/500.jpg"
+        },
+        "isHosted": false,
+        "pillarId": "pillar/news",
+        "pillarName": "News"
+      },
+      {
+        "id": "football/live/2022/nov/25/wales-v-iran-world-cup-2022-live",
         "type": "liveblog",
         "sectionId": "football",
         "sectionName": "Football",
-        "webPublicationDate": "2022-11-24T15:35:00Z",
-        "webTitle": "Portugal v Ghana: World Cup 2022 – live",
-        "webUrl": "https://www.theguardian.com/football/live/2022/nov/24/portugal-v-ghana-world-cup-2022-live",
-        "apiUrl": "https://content.guardianapis.com/football/live/2022/nov/24/portugal-v-ghana-world-cup-2022-live",
+        "webPublicationDate": "2022-11-25T10:31:37Z",
+        "webTitle": "Wales v Iran: World Cup 2022 – live",
+        "webUrl": "https://www.theguardian.com/football/live/2022/nov/25/wales-v-iran-world-cup-2022-live",
+        "apiUrl": "https://content.guardianapis.com/football/live/2022/nov/25/wales-v-iran-world-cup-2022-live",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/8a7d48b8f3b8d17eac63522a131ba8f605369c24/0_104_4340_2604/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/sport",
         "pillarName": "Sport"
       },
       {
-        "id": "world/live/2022/nov/24/russia-ukraine-war-live-zelenskiy-condemns-energy-terror-at-un-meeting-after-10-die-in-latest-mass-strikes",
+        "id": "football/2022/nov/25/iran-players-sing-national-anthem-world-cup-end-silent-protest-qatar",
+        "type": "article",
+        "sectionId": "football",
+        "sectionName": "Football",
+        "webPublicationDate": "2022-11-25T10:27:26Z",
+        "webTitle": "Iran players end silent protest and sing national anthem at World Cup ",
+        "webUrl": "https://www.theguardian.com/football/2022/nov/25/iran-players-sing-national-anthem-world-cup-end-silent-protest-qatar",
+        "apiUrl": "https://content.guardianapis.com/football/2022/nov/25/iran-players-sing-national-anthem-world-cup-end-silent-protest-qatar",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/b9e70f72e7a20e8272274e79db1fda5c23276e77/0_76_4637_2782/500.jpg"
+        },
+        "isHosted": false,
+        "pillarId": "pillar/sport",
+        "pillarName": "Sport"
+      },
+      {
+        "id": "world/live/2022/nov/25/russia-ukraine-war-live-russia-launches-further-strikes-on-zaporizhzhia-damaging-hospital-as-water-restored-in-kyiv",
         "type": "liveblog",
         "sectionId": "world",
         "sectionName": "World news",
-        "webPublicationDate": "2022-11-24T15:32:27Z",
-        "webTitle": "Russia-Ukraine war: Russian strikes risked ‘nuclear catastrophe’, says Ukraine energy chief; Moscow says 50 PoWs freed – live",
-        "webUrl": "https://www.theguardian.com/world/live/2022/nov/24/russia-ukraine-war-live-zelenskiy-condemns-energy-terror-at-un-meeting-after-10-die-in-latest-mass-strikes",
-        "apiUrl": "https://content.guardianapis.com/world/live/2022/nov/24/russia-ukraine-war-live-zelenskiy-condemns-energy-terror-at-un-meeting-after-10-die-in-latest-mass-strikes",
+        "webPublicationDate": "2022-11-25T10:24:01Z",
+        "webTitle": "Russia-Ukraine war live: Russia launches further strikes on Zaporizhzhia, damaging hospital, as water restored in Kyiv",
+        "webUrl": "https://www.theguardian.com/world/live/2022/nov/25/russia-ukraine-war-live-russia-launches-further-strikes-on-zaporizhzhia-damaging-hospital-as-water-restored-in-kyiv",
+        "apiUrl": "https://content.guardianapis.com/world/live/2022/nov/25/russia-ukraine-war-live-russia-launches-further-strikes-on-zaporizhzhia-damaging-hospital-as-water-restored-in-kyiv",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/d9ae08e394bcd6d26a295ebd181848f16c82530e/0_0_2040_1225/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/news",
         "pillarName": "News"
       },
       {
-        "id": "politics/live/2022/nov/24/uk-rail-strikes-royal-mail-industrial-action-unions-uk-politics-live-news",
+        "id": "world/2022/nov/25/french-team-build-replica-william-conqueror-warship",
+        "type": "article",
+        "sectionId": "world",
+        "sectionName": "World news",
+        "webPublicationDate": "2022-11-25T10:16:02Z",
+        "webTitle": "French team to build replica of William the Conqueror’s warship",
+        "webUrl": "https://www.theguardian.com/world/2022/nov/25/french-team-build-replica-william-conqueror-warship",
+        "apiUrl": "https://content.guardianapis.com/world/2022/nov/25/french-team-build-replica-william-conqueror-warship",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/431b9c7b0db4a83c2eac860642a803a70d235cba/218_0_4782_2870/500.jpg"
+        },
+        "isHosted": false,
+        "pillarId": "pillar/news",
+        "pillarName": "News"
+      },
+      {
+        "id": "politics/live/2022/nov/25/steve-barclay-nurses-strike-keir-starmer-nicola-sturgeon-uk-politics-live",
         "type": "liveblog",
         "sectionId": "politics",
         "sectionName": "Politics",
-        "webPublicationDate": "2022-11-24T15:29:47Z",
-        "webTitle": "Mick Lynch says RMT no closer to calling off rail strikes as transport minister states there is ‘deal to be done’ – UK politics live ",
-        "webUrl": "https://www.theguardian.com/politics/live/2022/nov/24/uk-rail-strikes-royal-mail-industrial-action-unions-uk-politics-live-news",
-        "apiUrl": "https://content.guardianapis.com/politics/live/2022/nov/24/uk-rail-strikes-royal-mail-industrial-action-unions-uk-politics-live-news",
+        "webPublicationDate": "2022-11-25T10:13:40Z",
+        "webTitle": "Health secretary Steve Barclay insists nurses getting ‘fair’ pay rise as strike dates announced – UK politics live",
+        "webUrl": "https://www.theguardian.com/politics/live/2022/nov/25/steve-barclay-nurses-strike-keir-starmer-nicola-sturgeon-uk-politics-live",
+        "apiUrl": "https://content.guardianapis.com/politics/live/2022/nov/25/steve-barclay-nurses-strike-keir-starmer-nicola-sturgeon-uk-politics-live",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/c8ce3e1366ee2a52eb93848e8a4718721815ba5e/0_155_3898_2339/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/news",
         "pillarName": "News"
       },
       {
-        "id": "business/live/2022/nov/24/pound-high-dollar-fed-uk-economy-recession-bank-of-england-business-live",
-        "type": "liveblog",
-        "sectionId": "business",
-        "sectionName": "Business",
-        "webPublicationDate": "2022-11-24T15:27:18Z",
-        "webTitle": "Bank of England deputy governor could vote to cut interest rates if inflation pressures ease – as it happened",
-        "webUrl": "https://www.theguardian.com/business/live/2022/nov/24/pound-high-dollar-fed-uk-economy-recession-bank-of-england-business-live",
-        "apiUrl": "https://content.guardianapis.com/business/live/2022/nov/24/pound-high-dollar-fed-uk-economy-recession-bank-of-england-business-live",
-        "isHosted": false,
-        "pillarId": "pillar/news",
-        "pillarName": "News"
-      },
-      {
-        "id": "culture/2022/nov/24/tamara-rojo-wades-into-bitter-row-over-london-arts-funding",
+        "id": "stage/2022/nov/25/the-mousetrap-first-broadway-production-after-70-years-agatha-christie",
         "type": "article",
         "sectionId": "stage",
         "sectionName": "Stage",
-        "webPublicationDate": "2022-11-24T15:20:11Z",
-        "webTitle": "Tamara Rojo wades into bitter row over London arts funding ",
-        "webUrl": "https://www.theguardian.com/culture/2022/nov/24/tamara-rojo-wades-into-bitter-row-over-london-arts-funding",
-        "apiUrl": "https://content.guardianapis.com/culture/2022/nov/24/tamara-rojo-wades-into-bitter-row-over-london-arts-funding",
+        "webPublicationDate": "2022-11-25T10:08:23Z",
+        "webTitle": "The Mousetrap: Agatha Christie’s West End hit to make Broadway debut after 70 years",
+        "webUrl": "https://www.theguardian.com/stage/2022/nov/25/the-mousetrap-first-broadway-production-after-70-years-agatha-christie",
+        "apiUrl": "https://content.guardianapis.com/stage/2022/nov/25/the-mousetrap-first-broadway-production-after-70-years-agatha-christie",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/41bf76db4c80d168e8c55a1bc3ccd8d7723ca432/0_936_4768_2860/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/arts",
         "pillarName": "Arts"
       },
       {
-        "id": "football/live/2022/nov/24/uruguay-v-south-korea-world-cup-2022-live",
-        "type": "liveblog",
-        "sectionId": "football",
-        "sectionName": "Football",
-        "webPublicationDate": "2022-11-24T15:18:30Z",
-        "webTitle": "Uruguay 0-0 South Korea: World Cup 2022 – as it happened",
-        "webUrl": "https://www.theguardian.com/football/live/2022/nov/24/uruguay-v-south-korea-world-cup-2022-live",
-        "apiUrl": "https://content.guardianapis.com/football/live/2022/nov/24/uruguay-v-south-korea-world-cup-2022-live",
-        "isHosted": false,
-        "pillarId": "pillar/sport",
-        "pillarName": "Sport"
-      },
-      {
-        "id": "world/2022/nov/24/journalist-under-strict-bail-terms-in-somalia-after-arrest-in-crackdown",
+        "id": "global-development/2022/nov/25/starvation-weapon-war-crimes-south-sudan-report",
         "type": "article",
-        "sectionId": "world",
-        "sectionName": "World news",
-        "webPublicationDate": "2022-11-24T15:15:46Z",
-        "webTitle": "Journalist under strict bail terms in Somalia after arrest in crackdown",
-        "webUrl": "https://www.theguardian.com/world/2022/nov/24/journalist-under-strict-bail-terms-in-somalia-after-arrest-in-crackdown",
-        "apiUrl": "https://content.guardianapis.com/world/2022/nov/24/journalist-under-strict-bail-terms-in-somalia-after-arrest-in-crackdown",
+        "sectionId": "global-development",
+        "sectionName": "Global development",
+        "webPublicationDate": "2022-11-25T10:01:50Z",
+        "webTitle": "Starvation being used as a weapon of war in South Sudan, report reveals",
+        "webUrl": "https://www.theguardian.com/global-development/2022/nov/25/starvation-weapon-war-crimes-south-sudan-report",
+        "apiUrl": "https://content.guardianapis.com/global-development/2022/nov/25/starvation-weapon-war-crimes-south-sudan-report",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/ec2da28adff1196d765e9eaf082e2a3626241146/0_333_5000_3000/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/news",
         "pillarName": "News"
       },
       {
-        "id": "football/2022/nov/24/uruguay-south-korea-world-cup-group-h-match-report",
+        "id": "media/2022/nov/25/fox-news-crime-coverage-decline-us-midterm-elections",
         "type": "article",
-        "sectionId": "football",
-        "sectionName": "Football",
-        "webPublicationDate": "2022-11-24T15:14:02Z",
-        "webTitle": "Frustration for Uruguay in stodgy goalless draw against South Korea",
-        "webUrl": "https://www.theguardian.com/football/2022/nov/24/uruguay-south-korea-world-cup-group-h-match-report",
-        "apiUrl": "https://content.guardianapis.com/football/2022/nov/24/uruguay-south-korea-world-cup-group-h-match-report",
-        "isHosted": false,
-        "pillarId": "pillar/sport",
-        "pillarName": "Sport"
-      },
-      {
-        "id": "world/2022/nov/24/row-breaks-out-favourite-eu-gulf-special-envoy-luigi-di-maio",
-        "type": "article",
-        "sectionId": "world",
-        "sectionName": "World news",
-        "webPublicationDate": "2022-11-24T15:11:18Z",
-        "webTitle": "Row breaks out over favourite for EU’s first-ever special envoy to Gulf",
-        "webUrl": "https://www.theguardian.com/world/2022/nov/24/row-breaks-out-favourite-eu-gulf-special-envoy-luigi-di-maio",
-        "apiUrl": "https://content.guardianapis.com/world/2022/nov/24/row-breaks-out-favourite-eu-gulf-special-envoy-luigi-di-maio",
+        "sectionId": "media",
+        "sectionName": "Media",
+        "webPublicationDate": "2022-11-25T10:00:05Z",
+        "webTitle": "Crime coverage on Fox News halved once US midterms were over",
+        "webUrl": "https://www.theguardian.com/media/2022/nov/25/fox-news-crime-coverage-decline-us-midterm-elections",
+        "apiUrl": "https://content.guardianapis.com/media/2022/nov/25/fox-news-crime-coverage-decline-us-midterm-elections",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/a1fc49e32cc0f4b0903e55186a112a6490f3274b/29_0_5510_3308/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/news",
         "pillarName": "News"
       },
       {
-        "id": "world/2022/nov/24/leftwing-brazilians-reclaim-football-jersey-bolsonaro-world-cup",
+        "id": "us-news/2022/nov/25/house-of-representatives-new-members-maxwell-frost-becca-balint-monica-de-la-cruz-mike-lawler-max-miller",
         "type": "article",
-        "sectionId": "world",
-        "sectionName": "World news",
-        "webPublicationDate": "2022-11-24T15:03:22Z",
-        "webTitle": "Leftwing Brazilians hope to reclaim football jersey from Bolsonaro movement",
-        "webUrl": "https://www.theguardian.com/world/2022/nov/24/leftwing-brazilians-reclaim-football-jersey-bolsonaro-world-cup",
-        "apiUrl": "https://content.guardianapis.com/world/2022/nov/24/leftwing-brazilians-reclaim-football-jersey-bolsonaro-world-cup",
+        "sectionId": "us-news",
+        "sectionName": "US news",
+        "webPublicationDate": "2022-11-25T10:00:05Z",
+        "webTitle": "Five new members of the House of Representatives to watch",
+        "webUrl": "https://www.theguardian.com/us-news/2022/nov/25/house-of-representatives-new-members-maxwell-frost-becca-balint-monica-de-la-cruz-mike-lawler-max-miller",
+        "apiUrl": "https://content.guardianapis.com/us-news/2022/nov/25/house-of-representatives-new-members-maxwell-frost-becca-balint-monica-de-la-cruz-mike-lawler-max-miller",
+        "fields": {
+          "thumbnail": "https://media.guim.co.uk/bec1268991b48b511118daa44af67c89fe128f01/0_273_8192_4918/500.jpg"
+        },
         "isHosted": false,
         "pillarId": "pillar/news",
         "pillarName": "News"
--- newsView.js
+++ newsView.js
@@ -9,16 +9,23 @@ class NewsView {
     const data = newsRaw.response.results;
 
     data.forEach((article) => {
+      const link = document.createElement("a");
       const mainHeader = document.createElement("h2");
       mainHeader.textContent = article.webTitle;
+      link.appendChild(mainHeader);
+      link.href = article.webUrl;
 
       const subHeader = document.createElement("h3");
       subHeader.textContent = article.sectionName;
 
+      const thumbnail = document.createElement("img");
+      thumbnail.src = article.fields.thumbnail;
+
       const newDiv = document.createElement("div");
       newDiv.className = "news";
-      newDiv.appendChild(mainHeader);
+      newDiv.appendChild(link);
       newDiv.appendChild(subHeader);
+      newDiv.appendChild(thumbnail);
 
       this.mainContainerEl.append(newDiv);
     });
